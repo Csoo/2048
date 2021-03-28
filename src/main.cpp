1c88a173e0b27bf741bcf22284727f78dc06ba34
Csongor <csocso96@gmail.com>
2021-03-28 17:14:26 +0200

    Set up player files

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,10 @@
 #include <iostream>
 #include "2048/direction.h"
 #include "2048/game.h"
+#include "Player/player.h"
 
 int main(int, char **) {
     Game2048 game;
-    game.start();
-    std::cout << game << '\n';
-    game.moveAll(DOWN);
-    std::cout << game << '\n';
-    game.moveAll(LEFT);
-    std::cout << game << '\n';
-    game.moveAll(DOWN);
-    std::cout << game << '\n';
-    game.moveAll(LEFT);
-    std::cout << game << '\n';
-    game.moveAll(DOWN);
-    std::cout << game << '\n';
-    game.moveAll(DOWN);
-    std::cout << game << '\n';
-    game.moveAll(LEFT);
-    std::cout << game << '\n';
-    game.moveAll(DOWN);
-    std::cout << game << '\n';
-    game.moveAll(DOWN);
-    std::cout << game << '\n';
-    game.moveAll(LEFT);
-    std::cout << game << '\n';
-    game.moveAll(DOWN);
-    std::cout << game << '\n';
+
     return 0;
 }
